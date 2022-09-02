9ce0614a84abe496756f16abf472afbd8f02df69
Mary Frye <deimospho@gmail.com>
2022-09-02 12:49:43 -0400

    ADDED modeSlice.js, DELETED dastaSlice.js, MODIFIED store, app, and photo.js

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
 function Photo() {
+    const mode = useSelector((state) => state.mode)
 
     return (
         <div className="Photo">
